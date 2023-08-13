2eede69060bf37bc61d5ece1682ce61dd3034b38
nurmuhammadrizky <nrmrizky5@gmail.com>
2023-08-14 03:50:30 +0700

    make a landing page with single introductory

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        violet: "hsl(257, 40%, 49%)",
+        softMagenta: "hsl(300, 69%, 71%)",
+      },
+    },
+  },
+  plugins: [],
+};
